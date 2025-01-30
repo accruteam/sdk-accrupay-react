d6d7db9b81993033195829b1c4661f967fdaadf0
Nicola Luongo <=>
2025-01-30 20:47:57 +0900

    change build config

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', '@stripe/react-stripe-js', '@stripe/stripe-js'],
+      external: ['react', 'react-dom'], //, '@stripe/react-stripe-js', '@stripe/stripe-js'],
       output: {
         globals: {
           react: 'React',
